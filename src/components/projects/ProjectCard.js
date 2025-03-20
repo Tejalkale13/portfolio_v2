8c8c0a8cf087439333baf829d82991db0e1975c8
Tejalkale13 <166007676+Tejalkale13@users.noreply.github.com>
2025-03-20 17:20:52 +0530

    Update ProjectCard.js

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import { VscAzure } from "react-icons/vsc";
+import { FaAws, FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import { 
   SiKubernetes, 
   SiTerraform, 
